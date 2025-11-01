70f89f11adcd1bc45be2dfb0655182c84d1a12f4
4444urka <149653447+4444urka@users.noreply.github.com>
2025-11-01 18:54:38 +1000

    refactor: улучшена логика загрузки лидерборда, заменен промис на async/await

--- frontend/src/hooks/useFetchLeaderboard.ts
+++ frontend/src/hooks/useFetchLeaderboard.ts
@@ -1,7 +1,6 @@
 import React from "react";
 import type { Me } from "../types/User";
 import { fetchLeaderboard } from "../api/stats/statsRequests";
-import { logger } from "../utils/logger";
 
 const useFetchLeaderboard = () => {
   const [leaderboard, setLeaderboard] = React.useState<Me[]>([]);
@@ -11,20 +10,19 @@ const useFetchLeaderboard = () => {
   React.useEffect(() => {
     const loadLeaderboard = async () => {
       setIsLoading(true);
-      await fetchLeaderboard()
-        .then((data) => {
-          setLeaderboard(data);
-          setIsLoading(false);
-        })
-        .catch((err) => {
-          setError(`Не удалось загрузить список лидеров: ${err.message}`);
-          setIsLoading(false);
-        });
+      try {
+        const data = await fetchLeaderboard();
+        setLeaderboard(data);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (err: any) {
+        setError(`Не удалось загрузить список лидеров: ${err.message}`);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
     loadLeaderboard();
   }, []);
-  logger.log("Лидерборд загружен", { count: leaderboard.length });
 
   return { leaderboard, isLoading, error };
 };
